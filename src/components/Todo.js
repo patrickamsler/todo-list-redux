4a78d3d9429f1dbb0d2f2902897f561d8b83fe5b
Patrick Amsler <patrick.amsler@zuhlke.com>
2018-10-22 07:02:11 -0700

    Function to delete a todo added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Todo = ({ onClick, completed, text }) => (
+const Todo = props => (
   <li
-    onClick={onClick}
+    onClick={props.onClick}
     style={{
-      textDecoration: completed ? 'line-through' : 'none'
+      textDecoration: props.completed ? 'line-through' : 'none'
     }}
   >
-    {text}
+    {props.text}
+    <button onClick={props.onDelete}>Delete</button>
   </li>
-)
+);
 
 Todo.propTypes = {
   onClick: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
   completed: PropTypes.bool.isRequired,
   text: PropTypes.string.isRequired
-}
+};
 
 export default Todo
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,23 +1,25 @@
 const todos = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }
-      ]
-    case 'TOGGLE_TODO':
-      return state.map(todo =>
-        (todo.id === action.id)
-          ? {...todo, completed: !todo.completed}
-          : todo
-      )
-    default:
-      return state
-  }
-}
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed: false
+                }
+            ];
+        case 'TOGGLE_TODO':
+            return state.map(todo =>
+                (todo.id === action.id)
+                    ? {...todo, completed: !todo.completed}
+                    : todo
+            );
+        case 'DELETE_TODO':
+            return [...state.filter(todo => action.id !== todo.id)];
+        default:
+            return state
+    }
+};
 
 export default todos
